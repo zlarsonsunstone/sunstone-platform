d2b2941ecd6b799d53316624f259d4336009f9b6
zlarsonsunstone <zack@sunstoneadvisors.co>
2026-05-04 09:59:51 -0400

    Add file upload helper for Surface Research

---
+++ src/lib/fileUpload.ts
@@ -0,0 +1,402 @@
+Allicent Technology LogoAllicent Technology 
+UEI: FN1PE72E1Q51 • CAGE: 8UZ62
+List
+Text
+Overview
+Parent Awardee
+FedTec 
+Name
+Allicent Technology
+Headquarters
+Reston, VA
+United States
+Joint Venture Member
+Prudent Technology
+Federal Registration and Certifications
+Legal Name
+ALLICENT TECHNOLOGY LLC
+UEI
+FN1PE72E1Q51
+CAGE Code
+8UZ62
+Primary NAICS
+541512 - Computer Systems Design Services
+SBA Certifications
+None
+Self Certifications
+Joint Venture Economically Disadvantaged Women Small Owned Business
+Joint Venture Women Owned Small Business
+Self-Certified Small Disadvantaged Business
+Woman Owned Business
+Woman Owned Small Business
+Entity Structure
+Corporate Entity (Not Tax Exempt)
+
+Show More
+People
+Vendor contacts at Allicent Technology
+
+5
+ Shown
+Search Keywords
+Name	Title / Contact Type	Contact		Last Seen
+Mona Kaur
+
+Electronic Government; More	 federal@allicenttechnology.com
+ +1 703-213-5775		12/27/25
+Sumaya Kohli
+
+Alt Government; More	
+12/27/25
+GSA Schedules and Price Lists
+47QTCA23D004Z
+Allicent Technology 
+PoP: 2/22/23 - 2/21/28
+Type: Multiple Award Schedule (MAS)
+View | Download
+ 
+Contract Vehicles
+Allicent Technology multi-award contract vehicles
+Search Keywords
+
+5
+ Shown
+Name	Type	Primary Agency	Ceiling	Award Date	Final Date	PY Awards
+Multiple Award Schedule (MAS)	GSA Schedule	Federal Acquisition Service Logo Federal Acquisition Service 	None	01/01/20	06/01/30	$35.1B
+Description Long-term government-wide contract that enables commercial firms to provide federal, state and local governments access to more than 11 million comercial products at volume discount pricing
+ 
+IDV Awards
+Allicent Technology indefinite delivery vehicle awards
+Search Keywords
+
+5
+ Shown
+Award ID	Awardee	Awarding Agency	Ceiling	Start	End	Modified
+47QTCA23D004Z 	Allicent Technology  	Federal Acquisition Service Logo Federal Acquisition Service 	$100.0K	02/22/23	02/21/28	03/27/25
+Description EO14042 this contract includes the Federal Acquisition Regulation (FAR) clause 52.223-99 ensuring adequate COVID-19 safety protocols for federal contractors - federal supply schedule contract
+
+Federal Supply Schedule
+PIID
+47QTCA23D004Z
+
+Open
+(1 year, 9 months remain)
+Unlinked Award
+Awarding Agency
+General Services Administration (GSA)
+Recipient
+ALLICENT TECHNOLOGY LLC
+1950 OPPORTUNITY WAY STE 1300
+RESTON, VA 20190-5495
+UNITED STATES
+Congressional District: VA-11
+Related Awards
+Parent Award Unique Key
+N/A
+Dates
+Today
+Start Date
+Feb 22, 2023
+Ordering Period End Date
+Feb 21, 2028
+$ Award Amounts
+Awards Under this IDV
+This IDV
+The information in this tab is pulled from the combined data of awards that reference this IDV, not the IDV itself. To see those awards, scroll to the table of awards under this IDV on this page.
+
+
+Chart Not Available
+Data in this instance is not suitable for charting
+
+Combined Outlayed Amounts
+$0.00
+Combined Obligated Amounts
+$0.00
+Combined Current Award Amounts
+$0.00
+Combined Potential Award Amounts
+$0.00
+Count of Awards Under this IDV
+
+
+View table of awards under this IDV
+Count of Child Contracts
+0
+Count of Child IDVs
+0
+Count of Grandchild Contracts
+0
+Total: 0
+Description
+EO14042 THIS CONTRACT INCLUDES THE FEDERAL ACQUISITION REGULATION (FAR) CLAUSE 52.223-99 ENSURING ADEQUATE COVID-19 SAFETY PROTOCOLS FOR FEDERAL CONTRACTORS - FEDERAL SUPPLY SCHEDULE CONTRACT
+
+North American Industry Classification System (NAICS)Code
+54 : Professional, Scientific, and Technical Services
+5415: Computer Systems Design and Related Services
+541511: Custom Computer Programming Services
+Product or Service Code(PSC)
+SERVICES
+D: IT AND TELECOM - INFORMATION TECHNOLOGY AND TELECOMMUNICATIONS
+DA: IT AND TELECOM - APLLICATIONS
+DA01: IT AND TELECOM - BUSINESS APPLICATION/APPLICATION DEVELOPMENT SUPPORT SERVICES (LABOR)
+IDV Activity
+Not Available
+No available data to display
+
+Federal Accounts
+This award has not been linked to any federal account.
+pedigree chart
+Orders Made Under this IDV
+Child Award Orders
+0
+Child IDV Orders
+0
+Grandchild Award Orders
+0
+No results found.
+Award History for this IDV
+Transaction History
+16
+Federal Account Funding
+0
+Modification Number
+
+
+Action Date
+
+
+Amount
+
+
+Action Type
+
+
+Transaction Description
+
+
+0
+02/22/2023
+$0
+--
+
+EO14042 THIS CONTRACT INCLUDES THE FEDERAL ACQUIS... Read More
+
+PA0002
+02/27/2023
+$0
+M: OTHER ADMINISTRATIVE ACTION
+
+FEDERAL SUPPLY SCHEDULE CONTRACT
+
+PA0003
+03/18/2024
+$0
+W: ENTITY ADDRESS CHANGE
+
+FEDERAL SUPPLY SCHEDULE CONTRACT
+
+PA0004
+04/24/2024
+$0
+M: OTHER ADMINISTRATIVE ACTION
+
+FEDERAL SUPPLY SCHEDULE CONTRACT
+
+PA0005
+10/26/2024
+$0
+M: OTHER ADMINISTRATIVE ACTION
+
+FEDERAL SUPPLY SCHEDULE CONTRACT
+
+PA0006
+03/05/2025
+$0
+M: OTHER ADMINISTRATIVE ACTION
+
+FEDERAL SUPPLY SCHEDULE CONTRACT
+
+PO0001
+02/22/2023
+$0
+M: OTHER ADMINISTRATIVE ACTION
+
+FEDERAL SUPPLY SCHEDULE CONTRACT
+
+PO0007
+03/27/2025
+$0
+N: LEGAL CONTRACT CANCELLATION
+
+FEDERAL SUPPLY SCHEDULE CONTRACT
+
+PSA815
+02/23/2023
+$0
+M: OTHER ADMINISTRATIVE ACTION
+
+FEDERAL SUPPLY SCHEDULE CONTRACT
+
+PSA856
+05/27/2023
+$0
+M: OTHER ADMINISTRATIVE ACTION
+
+FEDERAL SUPPLY SCHEDULE CONTRACT
+
+1-10 of 16 results
+Rows per page: 
+10
+
+1
+2
+
+Additional Information
+Collapse All
+Unique Award Key
+Unique Award Key
+CONT_IDV_47QTCA23D004Z_4732
+Award or IDV Flag
+Contract IDV
+Procurement Instrument Identifier (PIID)
+47QTCA23D004Z
+Submitting Agency Identifier Code
+4732
+Agency Details
+Awarding Agency
+General Services Administration (GSA)
+Awarding Sub-Agency
+Federal Acquisition Service
+Awarding Office
+GSA/FAS CENTER FOR IT SCHEDULE PROG
+Funding Agency
+General Services Administration (GSA)
+Funding Sub-Agency
+Federal Acquisition Service
+Funding Office
+GSA/FAS CENTER FOR IT SCHEDULE PROG
+Parent Award Details
+Parent Award Unique Key
+--
+Parent Award ID (Parent PIID)
+--
+Parent IDV Type
+--
+Parent IDV Agency Name
+--
+Parent IDV Sub-Agency Name
+--
+Multiple Or Single Parent Award IDV
+--
+Period of Performance
+Start Date
+02/22/2023
+Ordering Period End Date
+02/21/2028
+Legislative Mandates
+Clinger-Cohen Act Compliant
+NO
+Subject to Construction Wage Rate Requirements
+NOT APPLICABLE
+Subject to Labor Standards
+NO
+Subject to Materials, Supplies, Articles & Equipment
+NOT APPLICABLE
+Recipient Details
+Recipient
+ALLICENT TECHNOLOGY LLC
+Recipient Identifier
+FN1PE72E1Q51 (UEI )
+Parent Recipient
+ALLICENT TECHNOLOGY LLC
+Parent Recipient Identifier
+FN1PE72E1Q51 (UEI )
+Recipient Address
+1950 OPPORTUNITY WAY STE 1300
+RESTON, VA 20190-5495
+UNITED STATES
+Congressional District
+VA-11
+Business Types
+Category Business
+Corporate Entity Not Tax Exempt
+Joint Venture Women Owned Small Business
+Limited Liability Corporation
+Self-Certified Small Disadvantaged Business
+Small Business
+Special Designations
+U.S.-Owned Business
+Woman Owned Business
+Women Owned Small Business
+Acquisition Details
+Product or Service Code (PSC)
+DA01: IT AND TELECOM - BUSINESS APPLICATION/APPLICATION DEVELOPMENT SUPPORT SERVICES (LABOR)
+NAICS Code
+541511: CUSTOM COMPUTER PROGRAMMING SERVICES
+DoD Claimant Code
+--
+Sea Transportation
+--
+Competition Details
+Solicitation ID
+47QSMD20R0001
+Solicitation Procedures
+NP: NEGOTIATED PROPOSAL/QUOTE
+Number of Offers Received
+999
+Extent Competed
+A: FULL AND OPEN COMPETITION
+Not Competed Reason
+--
+Set-Aside Type
+NO SET ASIDE USED.
+Commercial Item Acquisition Procedures
+A: COMMERCIAL PRODUCTS/SERVICES
+Simplified Procedures for Certain Commercial Items
+NO
+Evaluated Preference
+NO PREFERENCE USED
+Fed Biz Opps
+YES
+Small Business Competitiveness Demonstration Program
+FALSE
+Additional Details
+IDV Type
+FSS
+IDC Type
+--
+National Interest Action
+--
+Multiple Or Single Award IDV
+MULTIPLE AWARD
+Cost or Pricing Data
+--
+Domestic or Foreign Entity
+U.S. OWNED BUSINESS
+Fair Opportunity Limited Sources
+--
+Foreign Funding
+NOT APPLICABLE
+Interagency Contracting Authority
+NOT APPLICABLE
+Major Program
+--
+Program Acronym
+--
+Subcontracting Plan
+B: PLAN NOT REQUIRED
+Multi Year Contract
+--
+Consolidated Contract
+NOT CONSOLIDATED
+Executive Compensation
+Officer 1
+--
+Officer 2
+--
+Officer 3
+--
+Officer 4
+--
+Officer 5
+--
